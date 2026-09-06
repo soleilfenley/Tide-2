93436e9b3a5c7645b80185f3779626ec8467a9b3
Soleil <60515000+flopsiedev@users.noreply.github.com>
2026-09-06 18:52:28 -0500

    Saving these before big source changes

--- src/main/java/com/li64/tide/compat/accessories/TrinketsCompat.java
+++ src/main/java/com/li64/tide/compat/accessories/TrinketsCompat.java
@@ -3,7 +3,11 @@ package com.li64.tide.compat.accessories;
 
 import com.li64.tide.data.TideTags;
 import com.li64.tide.data.informational.FishingInfoManager;
+//? if =26.2 {
+/*import eu.pb4.trinkets.api.TrinketsApi;
+*///?} else {
 import dev.emi.trinkets.api.TrinketsApi;
+//?}
 import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.item.Item;
 
@@ -13,11 +17,19 @@ public class TrinketsCompat {
     public static void addInformationalItems(Player player, ArrayList<Item> items) {
         var component = TrinketsApi.getTrinketComponent(player);
         if (component.isEmpty()) return;
+        //? if =26.2 {
+        /*component.get().equipped(stack -> stack.is(TideTags.Items.INFORMATIONAL), true)
+                .forEach(access -> {
+                    Item item = access.getStack().getItem();
+                    FishingInfoManager.addInfoItem(items, item);
+                });
+        *///?} else {
         component.get().getEquipped(stack -> stack.is(TideTags.Items.INFORMATIONAL))
                 .forEach(tuple -> {
                     Item item = tuple.getB().getItem();
                     FishingInfoManager.addInfoItem(items, item);
                 });
+        //?}
     }
 }
 //?}

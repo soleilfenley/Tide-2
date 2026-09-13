bcc305158f8ec06e4ec9733c18908ac2399b6535
Soleil <60515000+flopsiedev@users.noreply.github.com>
2026-09-12 20:48:28 -0500

    Create TideParticleTypes.java

---
+++ src/main/java/com/li64/tide/registries/particles/TideParticleTypes.java
@@ -0,0 +1,33 @@
+package com.li64.tide.registries.particles;
+
+import com.li64.tide.Tide;
+import net.minecraft.core.particles.SimpleParticleType;
+import net.minecraft.core.registries.BuiltInRegistries;
+
+public class TideParticleTypes {
+        //? if >=26.2 {
+        public static final SimpleParticleType VOID_RIPPLE_LARGE = register("void_ripple_large", new TideSimpleParticleType(true));
+        public static final SimpleParticleType VOID_RIPPLE_SMALL = register("void_ripple_small", new TideSimpleParticleType(true));
+        public static final SimpleParticleType MAGIC_CHAIN = register("magic_chain", new TideSimpleParticleType(true));
+        //?} else {
+        /*
+        public static final SimpleParticleType VOID_RIPPLE_LARGE = register("void_ripple_large", new SimpleParticleType(true));
+        public static final SimpleParticleType VOID_RIPPLE_SMALL = register("void_ripple_small", new SimpleParticleType(true));
+        public static final SimpleParticleType MAGIC_CHAIN = register("magic_chain", new SimpleParticleType(true));
+        */
+        //?}
+        
+        public static SimpleParticleType register(String key, SimpleParticleType particleType) {
+                return Tide.PLATFORM.register(BuiltInRegistries.PARTICLE_TYPE, Tide.resource(key), particleType);
+        }
+        
+        public static void init() {}
+
+        //? if >=26.2 {
+        private static class TideSimpleParticleType extends SimpleParticleType {
+                protected TideSimpleParticleType(boolean overrideLimiter) {
+                        super(overrideLimiter);
+                }
+        }
+        //?}
+}
